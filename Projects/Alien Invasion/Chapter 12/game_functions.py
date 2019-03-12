d1ee1276b60573668af02e426882b7524bb4cff4
larry <trance1100@gmail.com>
2019-03-11 22:54:49 -0400

    aliens and bullets

--- Projects/Alien Invasion/Chapter 12/game_functions.py
+++ Projects/Alien Invasion/Chapter 12/game_functions.py
@@ -7,10 +7,16 @@ def check_keydown_events(event, ai_settings, screen, ship, bullets):
     elif event.key == pygame.K_LEFT:
         ship.moving_left = True
     elif event.key == pygame.K_SPACE:
-        # Create a new bullet and add it to the bullets group 
+        fire_bullet(ai_settings, screen, ship, bullets)
+    elif event.key == pygame.K_q:
+        sys.exit()
+
+def fire_bullet(ai_settings, screen, ship, bullets):
+    """ Fire a bullet if limit not reached yet. """
+    # Create a new bullet and add it to the bullets group.
+    if len(bullets) < ai_settings.bullets_allowed:
         new_bullet = Bullet(ai_settings, screen, ship)
         bullets.add(new_bullet)
-
 def check_keyup_events(event, ship):
     if event.key == pygame.K_RIGHT:
         ship.moving_right = False
@@ -32,7 +38,21 @@ def update_screen(ai_settings, screen, ship, bullets):
     """ Update images on the screen and flip to the new screen. """
     # Redraw the screen during each pass through the loop.
     screen.fill(ai_settings.bg_color)
+    # Redraw all bullet behind ship and aliens.
+    for bullet in bullets.sprites():
+        bullet.draw_bullet()
     ship.blitme()
 
     # Make the most recently drawn screen visible.
     pygame.display.flip()
+
+def update_bullets(bullets):
+    """ Update position of bullets and get rid of old bullets. """
+    # Update bullet positions.
+    bullets.update()
+
+    # Get rid of bullets that have disappeared.
+    for bullet in bullets.copy():
+            if bullet.rect.bottom <= 0:
+                    bullets.remove(bullet)
+    print(len(bullets))
